ee7fb23030905fa3b341e70f040bb27d0a37e95c
RickyRAV <rickyraveanu02@gmail.com>
2023-06-09 21:33:10 +0200

    FinishingInsertingINTOAllTables

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ mydb = mysql.connector.connect(
     database="F1Spectrum"
 )
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor(buffered=True)
 
 # Fetch data from Ergast API
-response = requests.get("http://ergast.com/api/f1/2023.json")
+response = requests.get("http://ergast.com/api/f1/2022.json")
 data = response.json()
 
 # Insert data into Seasons table
-season_year = 2023
+season_year = 2022
 mycursor.execute("INSERT INTO Seasons (Year) VALUES (%s)", (season_year,))
 mydb.commit()
 
@@ -37,12 +37,63 @@ for race in data['MRData']['RaceTable']['Races']:
                          (circuit['circuitName'], circuit['Location']['locality']))
         mydb.commit()
         circuit_id = mycursor.lastrowid
+    else:
+        circuit_id = circuit_id[0]  # Extract single value from tuple
 
     # Insert race
     mycursor.execute("INSERT INTO Races (SeasonID, CircuitID, Date, Round) VALUES (%s, %s, %s, %s)",
                      (season_id, circuit_id, race['date'], race['round']))
     mydb.commit()
 
+# Fetch constructors data from Ergast API
+response = requests.get("http://ergast.com/api/f1/2022/constructors.json")
+data = response.json()
+
+# Insert data into Constructors table
+for constructor in data['MRData']['ConstructorTable']['Constructors']:
+    mycursor.execute("INSERT INTO Constructors (Name, Nationality) VALUES (%s, %s)",
+                     (constructor['name'], constructor['nationality']))
+    mydb.commit()
+
+# Fetch drivers data from Ergast API
+response = requests.get("http://ergast.com/api/f1/2022/results.json")
+data = response.json()
+
+# Insert data into Drivers table
+for race in data['MRData']['RaceTable']['Races']:
+    for result in race['Results']:
+        driver = result['Driver']
+        constructor = result['Constructor']
+        # Get the constructor id for the driver
+        mycursor.execute("SELECT ConstructorID FROM Constructors WHERE Name = %s", (constructor['name'],))
+        constructor_id = mycursor.fetchone()[0]
+
+        mycursor.execute("INSERT INTO Drivers (ConstructorID, Name, Nationality) VALUES (%s, %s, %s)",
+                         (constructor_id, driver['givenName'] + ' ' + driver['familyName'], driver['nationality']))
+        mydb.commit()
 
 
-# And so on for Constructors, Drivers, Results, and Telemetry tables...
+
+# Fetch results data from Ergast API
+response = requests.get("http://ergast.com/api/f1/2022/results.json")
+data = response.json()
+
+# Insert data into Results table
+for race in data['MRData']['RaceTable']['Races']:
+    # Get the race id
+    mycursor.execute("SELECT RaceID FROM Races WHERE Date = %s", (race['date'],))
+    race_id = mycursor.fetchone()[0]
+
+    for result in race['Results']:
+        # Get the driver id and constructor id
+        mycursor.execute("SELECT DriverID FROM Drivers WHERE Name = %s",
+                         (result['Driver']['givenName'] + ' ' + result['Driver']['familyName'],))
+        driver_id = mycursor.fetchone()[0]
+
+        mycursor.execute("SELECT ConstructorID FROM Constructors WHERE Name = %s", (result['Constructor']['name'],))
+        constructor_id = mycursor.fetchone()[0]
+
+        mycursor.execute(
+            "INSERT INTO Results (RaceID, DriverID, ConstructorID, GridPosition, FinalPosition, Points) VALUES (%s, %s, %s, %s, %s, %s)",
+            (race_id, driver_id, constructor_id, result['grid'], result['position'], result['points']))
+        mydb.commit()
